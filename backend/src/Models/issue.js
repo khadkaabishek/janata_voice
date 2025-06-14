478c073d31e22dec9f79ef089a9f6863ae98e1ca
khadkaabishek <abishekkhadka36@gmail.com>
2025-06-15 03:44:14 +0545

    little done

--- backend/src/Models/issue.js
+++ backend/src/Models/issue.js
@@ -74,11 +74,11 @@ const issueSchema = new mongoose.Schema(
     },
 
     // ✅ User reference
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+    // user: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "User",
+    //   required: true,
+    // },
   },
   { timestamps: true }
 );
